67a1dea7238a39af50dd764b486c32708d982623
Joshua Gomberoff <joshuagomberoff@gmail.com>
2021-05-14 09:47:48 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+require("dotenv").config();
+
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port: ${PORT} 🚀`);
+});
